4d0745cd011f1b769bdaea1cf76b307a28950cf7
StijnAvdL <stijn@orikami.nl>
2019-03-19 14:20:13 +0100

    always inAppBroswer

--- src/agent/index.js
+++ src/agent/index.js
@@ -3,9 +3,9 @@ var WebViewAgent = require('./webview');
 
 module.exports = function getAgent(callback) {
   return BrowserAgent.isAvailable(function (available) {
-    if (available) {
-      return callback(null, new BrowserAgent());
-    }
+    // if (available) {
+    //   return callback(null, new BrowserAgent());
+    // }
     return callback(null, new WebViewAgent());
   });
 };
